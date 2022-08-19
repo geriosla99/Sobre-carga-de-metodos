234549e7a00d71b98de6386851bfd13bb2ece005
geriosla99 <geriosla@gmail.com>
2022-08-18 20:03:56 -0500

    Agregar archivos de proyecto.

---
+++ Sobre carga de metodos/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Sobre_carga_de_metodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variable para recibir desde "Concatenar"
+            string nombreYApellido;
+
+            //Instancia
+            Nombres nombres = new Nombres();
+
+            nombreYApellido = nombres.Concatenar("Leticia", "Guzmán", "Valdez");
+            Random random = new Random();
+            //Mostrando las cadenas concatenadas
+            Console.WriteLine(random.Next(10, 20));
+        }
+    }
+    class Nombres
+    {
+        //Método
+        public string Concatenar(string nombrePa, string apellidoPa)
+        {
+            string nombreApellido;
+            nombreApellido = nombrePa + " " + apellidoPa;
+            return nombreApellido;
+        }
+
+        //Sobrecarga del metodo concatenar
+        public string Concatenar(string nombrePa, string apellidoPa, string segundoApellidoPa)
+        {
+            string nombreApellido;
+            nombreApellido = nombrePa + " " + apellidoPa+ " "+ segundoApellidoPa;
+            return nombreApellido;
+        }
+    }
+}
